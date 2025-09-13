530f409d74eac0f8cf930fbd3e310bc2f5c3c7f8
RailMLearning <rail.ml.dev@gmail.com>
2025-09-13 18:44:33 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-def sum_finder(nums, target):
-    for i in range(0, len(nums)):
-        for j in range(i, len(nums)):
-            if i == j:
-                continue
-            if nums[i] + nums[j] == target:
-                return [i,j]
-    return None
