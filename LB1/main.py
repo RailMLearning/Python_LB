964db67e55ec0f5cefb12e4a31e111529a99930e
RailMLearning <rail.ml.dev@gmail.com>
2025-09-13 18:48:15 +0300

    Update main.py

--- LB1/main.py
+++ LB1/main.py
@@ -1,8 +1,11 @@
 def sum_finder(nums, target):
+    if len(nums)==1:
+        return None
     for i in range(0, len(nums)):
         for j in range(i, len(nums)):
             if i == j:
                 continue
             if nums[i] + nums[j] == target:
                 return [i,j]
+
     return None
