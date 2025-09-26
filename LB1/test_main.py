f8e9a4344a1aefcf7323c4159ce3384317e719f0
RailMLearning <rail.ml.dev@gmail.com>
2025-09-26 12:27:25 +0300

    Update test_main.py

--- LB1/test_main.py
+++ LB1/test_main.py
@@ -1,30 +1,37 @@
 import unittest
-from main import sum_finder
+from main1 import sum_finder
 
 
 # Тесты
 class TestMath(unittest.TestCase):
-    def test_sum_finder_zero(self):
+    def test_1(self):
         self.assertEqual(sum_finder([2,7,11,15], 9), [0,1])
 
-    def test_sum_finderh_zero(self):
+    def test_2(self):
         self.assertEqual(sum_finder([3,2,4], 6), [1,2])
 
-    def test_sum_findger_zero(self):
+    def test_3(self):
         self.assertEqual(sum_finder([3,3], 6), [0,1])
 
-    def test_sum_finder_positive(self):
+    def test_4(self):
         self.assertEqual(sum_finder([2,3], 5), [0,1])
 
-    def test_sum_finder_negative(self):
+    def test_5(self):
         self.assertEqual(sum_finder([-1, -3],-4), [0,1])
 
-    def test_sum_finder_zkero(self):
+    def test_6(self):
         self.assertEqual(sum_finder([1,24,7], 5), None)
 
-    def test_sum_finder(self):
+    def test_7(self):
         self.assertEqual(sum_finder([1], 5), None)
+
+    def test_8(self):
+        self.assertEqual(sum_finder([1,999999999,99999999,99999999,4], 5), [0,4])
+
+    def tefst_9(self):
+        self.assertEqual(sum_finder([-234,-234,-1,-5,-5], 5), None)
 
 
 if __name__ == '__main__':
+
     unittest.main()
